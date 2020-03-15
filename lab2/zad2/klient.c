55d00f55d162d33baec3e823831467c241a2c7d8
Marek Skrzypkowski <marek.skrzypkowski98@gmail.com>
2020-03-15 16:07:26 +0100

    zadanie 2

--- lab2/zad2/klient.c
+++ lab2/zad2/klient.c
@@ -1,29 +1,33 @@
 #include <fcntl.h> 
 #include <unistd.h> 
 #include <stdio.h>
-#define WIADOMOSC 512
+#define WIADOMOSC 1024
 
-//Nawiazywanie polaczenia z serwerem
-void polacz()
-{	
+
+int main(int argc, char *argv[]) 
+{
+	//pobranie nazwy klienta
+	char uzytkownik[100] = {};
+	getlogin_r(uzytkownik, 100);
+		
+	printf("KLIENT - Uruchomiono program\n");
+	printf("(Zalogowany jako:%s)\n" , uzytkownik);
+	
+	//łączenie z serwerem
 	while (open("lockfile",O_CREAT|O_EXCL, 0711)==-1)
     {
        printf("Serwer zajety, prosze czekac\n\n");	
-       sleep(3);
+       sleep(2);
     }
-}
 
-//Procedura wysylania tresci wiadomosci do serwera (bufor dane.txt)
-void wyslij()
-{
+	//wysyłanie wiadomości do serwera
 	int dane;
-	char bufor[WIADOMOSC] ={0x00,};
-	char znak[1] = {0x00,};
+	char bufor_dane[WIADOMOSC] ={};
+	char znak[1] = {};
 	int counter = 0;
 	
-	char uzytkownik[100] = {0x00,};
-	getlogin_r(uzytkownik, 100);
-	char output[1024] = {0x00,};
+	
+	char output[1200] = {}; //wiadomość plus nagłówek z nazwą użytkownika
 	
 	while((dane = open("dane.txt", O_RDWR|O_CREAT|O_APPEND, 0711)) < 0)
 	{
@@ -38,22 +42,20 @@ void wyslij()
 			read(0,znak,1);
 			if(znak[0] == 27)
             {
-				counter = sprintf(output, "%s:\n%s" , uzytkownik, bufor);
+				//liczba zapisanych znaków do stringa
+				counter = sprintf(output, "%s:\n%s" , uzytkownik, bufor_dane);
 				write(dane, output, counter);
 				break;
 			}
-				bufor[counter] = znak[0];
+				bufor_dane[counter] = znak[0];
 				counter++;
 				
 		}
 	}
-}
-
-//Odbieranie wiadomosci zwrotnej od serwera (bufor wyniki.txt)
-void odbierz()
-{
+		
+	//odbieranie wiadomości zwrotnej od serwera
 	int wyniki;
-	char bufor[WIADOMOSC] ={0x00,};
+	char bufor_wyniki[WIADOMOSC] ={};
 
 	while((wyniki = open("wyniki.txt", O_RDWR)) < 0)
 	{
@@ -61,43 +63,15 @@ void odbierz()
 		
 	if(wyniki != 0)
 	{
-		while(read(wyniki,bufor,512) < 1)
+		while(read(wyniki,bufor_wyniki,1024) < 1)
 		{
 		}
-		printf("WIADOMOSC Z SERWERA:\n%s", bufor);
+		printf("WIADOMOSC Z SERWERA:\n%s", bufor_wyniki);
 		close(wyniki);
 	}
-}
 
-//Oczyszczanie buforow wyniki i dane.txt (faktyczne kasowanie plikow tekstowych)
-void rozlacz()
-{
+	//czyszczenie bufora
 	unlink("wyniki.txt");
-	
-}
-
-//Glowna funkcja programu - pobieranie nazwy uzytkownika i wywolywanie procedur wysylki i odbioru wiadomosci wraz z laczenie i rozlaczaniem z serwerem  
-int main(int argc, char *argv[]) 
-{
-	char uzytkownik[100] = {0x00,};
-	getlogin_r(uzytkownik, 100);
-	
-	/*
-	char sciezka_programu[200];
-	getcwd(sciezka_programu, 200);
-	printf("pwd -> %s\n", sciezka_programu);	
-	*/
-		
-	printf("Witaj w komunikatorze tekstowym - KLIENT\n");
-	printf("(Zalogowany jako:%s)\n" , uzytkownik);
-
-		polacz();
-		
-		wyslij();
-		
-		odbierz();
-
-		rozlacz();
 
 	getchar();
     return 0;
