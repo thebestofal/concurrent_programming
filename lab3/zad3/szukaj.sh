4a65a34934c936ae5f9e112243c846113837011e
Marek Skrzypkowski <marek.skrzypkowski98@gmail.com>
2020-03-16 19:37:58 +0100

    added zad3 in lab3

--- lab3/zad3/szukaj.sh
+++ lab3/zad3/szukaj.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-
+declare -i counter
+counter=0
+children=""
 for i in $2/*
 do
 	if [ -f $i ] && [[ $i =~ .*/$1 ]]
 	then
 		echo $i
+		((counter=counter+1))
 	elif [ -d $i ]
 	then
-		./szukaj.sh $1 $i
-	
+		./szukaj.sh $1 $i "null" &
+		children+=($!)
 	fi
 done
+
+for i in ${children[@]} #all values of array
+do
+	if [ $i ]
+	then
+		wait $i
+		counter=$((counter+$?))
+	fi
+done
+
+if [ -z $3 ]
+then
+	if [ $counter -eq 0 ]
+	 then
+		echo "Nie znaleziono"
+	fi
+fi
+
